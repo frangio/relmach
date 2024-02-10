cab68699779a174b6b7859124d4fd69b042abfb4
Francisco Giordano <fg@frang.io>
2024-02-10 02:23:05 -0300

    reorder job components

--- lib/Machine.ml
+++ lib/Machine.ml
@@ -2,14 +2,14 @@ open Types
 open Result
 
 type job =
-  | Eval of term * env * cont * universe
-  | Return of result * cont * universe
+  | Eval of term * env * universe * cont
+  | Return of result * universe * cont
 
 type pool = job Queue.t
 
 let init (t : term) : pool =
   let p = Queue.create () in
-  Queue.add (Eval (t, [], Ret, Universe.root ())) p;
+  Queue.add (Eval (t, [], Universe.root (), Ret)) p;
   p
 
 let run (p : pool) : result Seq.t =
@@ -20,8 +20,8 @@ let run (p : pool) : result Seq.t =
     let sv = Unifier.find u.unifier sv in
     let skq = PQueue.snoc sk skq in
     match sv with
-    | Sym _ -> enqueue (Return (Result (sv, skq), k, u))
-    | _ -> enqueue (Return (value sv, Resume (skq, k), u))
+    | Sym _ -> enqueue (Return (Result (sv, skq), u, k))
+    | _ -> enqueue (Return (value sv, u, Resume (skq, k)))
   in
 
   let rec apply k op l r u =
@@ -30,7 +30,7 @@ let run (p : pool) : result Seq.t =
     else
       match op with
       | Seq ->
-          enqueue (Return (r, k, u))
+          enqueue (Return (r, u, k))
       | Unif | App when is_stuck r ->
           try_unstuck k r (SRightOf (op, l)) u
       | Unif ->
@@ -41,7 +41,7 @@ let run (p : pool) : result Seq.t =
               Universe.kill u
           | Some g ->
               u.unifier <- g;
-              enqueue (Return (value (make_struct "ok"), k, u))
+              enqueue (Return (value (make_struct "ok"), u, k))
           end
       | App ->
           let Result (lv, _) = l in
@@ -50,17 +50,17 @@ let run (p : pool) : result Seq.t =
           | Sym _ ->
               begin match Unifier.find u.unifier lv with
               | Sym _ as x ->
-                  enqueue (Return (stuck x (SLeftOf (App, r)), k, u))
+                  enqueue (Return (stuck x (SLeftOf (App, r)), u, k))
               | l' ->
                   apply k op (value l') r u
               end
           | Struct _ ->
-              enqueue (Return (value (cons_struct lv rv), k, u))
+              enqueue (Return (value (cons_struct lv rv), u, k))
           | Clos (_, _, [body], e) ->
-              enqueue (Eval (body, rv :: e, k, u))
+              enqueue (Eval (body, rv :: e, u, k))
           | Clos (_, _, bodies, e) ->
               let app body =
-                enqueue (Eval (body, rv :: e, k, Universe.branch u))
+                enqueue (Eval (body, rv :: e, Universe.branch u, k))
               in
               List.iter app bodies
           end
@@ -74,49 +74,49 @@ let run (p : pool) : result Seq.t =
     match Queue.take p with
     | exception Queue.Empty ->
         Seq.Nil
-    | Eval (_, _, _, u) | Return (_, _, u) when Universe.is_dead u ->
+    | Eval (_, _, u, _) | Return (_, u, _) when Universe.is_dead u ->
         next ()
-    | Eval (Bin (op, l, r), e, k, u) ->
+    | Eval (Bin (op, l, r), e, u, k) ->
         let f = { op; k; left = []; right = [] } in
-        enqueue (Eval (l, e, LeftOf f, u));
-        enqueue (Eval (r, e, RightOf f, u));
+        enqueue (Eval (l, e, u, LeftOf f));
+        enqueue (Eval (r, e, u, RightOf f));
         next ()
-    | Eval (Nu (x, t), e, k, u) ->
+    | Eval (Nu (x, t), e, u, k) ->
         let x = make_symbol x in
-        enqueue (Eval (t, x :: e, k, u));
+        enqueue (Eval (t, x :: e, u, k));
         next ()
-    | Eval (Lam (x, t), e, k, u) ->
+    | Eval (Lam (x, t), e, u, k) ->
         let v = make_closure x t e in
-        enqueue (Return (value v, k, u));
+        enqueue (Return (value v, u, k));
         next ()
-    | Eval (Const c, _, k, u) ->
+    | Eval (Const c, _, u, k) ->
         let v = make_struct c in
-        enqueue (Return (value v, k, u));
+        enqueue (Return (value v, u, k));
         next ()
-    | Eval (Var i, e, k, u) ->
+    | Eval (Var i, e, u, k) ->
         let v = List.nth e i in
-        enqueue (Return (value v, k, u));
+        enqueue (Return (value v, u, k));
         next ()
-    | Return (r, Resume (skq, k), u) ->
+    | Return (r, u, Resume (skq, k)) ->
         begin match r, PQueue.head skq, PQueue.tail skq with
         | exception PQueue.Empty ->
-            enqueue (Return (r, k, u))
+            enqueue (Return (r, u, k))
         | rl, SLeftOf (op, rr), skq'
         | rr, SRightOf (op, rl), skq' ->
             let k' = Resume (skq', k) in
             apply k' op rl rr u
         end;
         next ()
-    | Return (l, LeftOf fr, lu) ->
+    | Return (l, lu, LeftOf fr) ->
         (* todo: optimization for prematurely stuck results *)
         List.iter (fun (r, ru) -> join_apply fr.k fr.op l r lu ru) fr.right;
         fr.left <- (l, lu) :: fr.left;
         next ()
-    | Return (r, RightOf fr, ru) ->
+    | Return (r, ru, RightOf fr) ->
         List.iter (fun (l, lu) -> join_apply fr.k fr.op l r lu ru) fr.left;
         fr.right <- (r, ru) :: fr.right;
         next ()
-    | Return (r, Ret, u) ->
+    | Return (r, u, Ret) ->
         let r' = map_result (Unifier.subst u.unifier) r in
         Seq.Cons (r', next)
   in
