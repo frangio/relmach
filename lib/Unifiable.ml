078f5ff492fca0f282751e35e2ff3864e1cb90e0
Francisco Giordano <fg@frang.io>
2024-02-11 13:01:06 -0300

    fix struct unification

--- lib/Unifiable.ml
+++ lib/Unifiable.ml
@@ -79,7 +79,7 @@ module Make (P : Params) = struct
         match v, w with
         | Sym _, _ | _, Sym _ -> unify_sym v w u
         | Clos _, Clos _ -> if v == w then Some u else None
-        | Struct (_, c, vs), Struct (_, d, ws) -> if c == d then unify_list vs ws u else None
+        | Struct (_, c, vs), Struct (_, d, ws) -> if c = d then unify_list vs ws u else None
         | _, _ -> None
 
       and unify_list vs ws u =
