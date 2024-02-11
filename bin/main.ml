4011a835f7162f579888b94051dda10ddd5e852b
Francisco Giordano <fg@frang.io>
2024-02-11 13:15:08 -0300

    add cli arg

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,15 @@
 open Relmach
 
-let () = Printexc.record_backtrace true
-
 let read_file file = In_channel.with_open_text file In_channel.input_all
 
+let file_path = try Sys.argv.(1) with _ -> failwith "missing input file"
+
+let () = Printexc.record_backtrace true
+
 let () =
-  let source = read_file "test.sml" in
+  let source = read_file file_path in
   match Parse.parse source with
   | Ok p ->
-      Pp.print_term p;
-      print_newline ();
       let rs = Machine.run (Machine.init p) in
       Seq.iter
         (fun r ->
