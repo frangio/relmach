bc8daf7e5e50927fb011877e2afc78c43c95ea3f
Francisco Giordano <fg@frang.io>
2024-10-06 13:07:42 -0300

    add catenable list module

---
+++ lib/CList.mli
@@ -0,0 +1,14 @@
+type 'a t
+
+exception Empty
+
+val empty : 'a t
+val is_empty : 'a t -> bool
+
+val snoc : 'a -> 'a t -> 'a t
+val append : 'a t -> 'a t -> 'a t
+
+val head : 'a t -> 'a
+val tail : 'a t -> 'a t
+
+val map : ('a -> 'b) -> 'a t -> 'b t
